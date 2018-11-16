6a5de19e3db67765c688b44904d836ab110c3967
kmyles82 <kmyles82@gmail.com>
2018-11-16 12:11:00 -0600

    adding navbar and script

--- index.php
+++ index.php
@@ -1,30 +1,16 @@
-<?php
 
-require'config/config.php';
-
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>SpeakEasy</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-	<!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<!-- Compiled and minified CSS -->
-<!--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">-->
-<!--	<link rel="stylesheet" href="styles.css" type="text/css">-->
-</head>
-<body>
-   
+   <?php
+      include('includes/header.php');
+      // session_destroy();
+      
+   ?>
    
 	
-   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
+   
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="script.js"></script>
 </body>
 </html>
